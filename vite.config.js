543a831110098d336579e8da123be418bb34c83b
bezriche-ramy <ramybezriche@gmai.com>
2025-02-21 18:07:37 +0100

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/newport/', // Replace 'newport' with your repository name
+  base: '/',
+  build: {
+    outDir: 'dist',
+  }
 });
