3ba385e3b6ddca473f302ae8c3f84691ad8e18e4
bezriche-ramy <ramybezriche@gmail.com>
2025-05-01 22:09:51 +0100

    Mise à jour du site

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,5 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'animation-vendor': ['framer-motion'],
-          'icon-vendor': ['react-icons'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-    cssCodeSplit: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'framer-motion']
   }
 });
